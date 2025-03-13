8ba6d587e185ca30051e768bef7e5c1322229eb8
Aditi Pattanshetti <aditimahant@gmail.com>
2025-03-13 18:21:44 +0000

    changing dijkstras

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -5,7 +5,7 @@
 vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous) {
     int n = G.numVertices;
     vector<int> distances(n, INF);
-    previous.resize(n, -1);
+    previous.assign(n, -1);
     vector<bool> visited(n, false);
 
     priority_queue<Node, vector<Node>, greater<Node>> pq;
